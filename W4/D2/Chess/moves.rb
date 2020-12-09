cfd3d39b76c9115ebb23146bfd3afe87b14c5624
appkenny <kzeng.95@gmail.com>
2020-12-09 14:36:55 -0800

    Worked on cardinal moves

--- W4/D2/Chess/moves.rb
+++ W4/D2/Chess/moves.rb
@@ -5,11 +5,17 @@ module Cardinal
     VERTICAL = [0, 1]
 
     def horizontal_moves(pos)
-        all_possible_moves_hor = []
+        all_possible_moves = []
+                
+        (0..7).each do |col|
+            all_possible_moves << [pos[0], col] if pos[1] != col
+        end
         
+        all_possible_moves
     end
 
-    def vertical_moves
+    def vertical_moves(pos)
+        possible_vertical_moves = []
     end
 end
 
