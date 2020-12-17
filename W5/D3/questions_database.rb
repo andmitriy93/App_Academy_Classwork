0e0148e2512de6c3da5533d2f889fbe8aed7a5c4
Edwin Cheng <edwincheng3@gmail.com>
2020-12-16 16:39:25 -0800

    Working on Ruby part

--- W5/D3/questions_database.rb
+++ W5/D3/questions_database.rb
@@ -11,31 +11,110 @@ class QuestionsDatabase < SQLite3::Database
   end
 end
 
-# users questions question_follows replies question_likes
+
+
+# Tables to Make into Classes
+
+# 1. users (finished)
+# 2. questions (WIP)
+# 3. question_follows 
+# 4. replies question_likes
+
 class User
 
-    def self.all
-        data = QuestionsDatabase.instance.execute("SELECT * FROM users")
-        data.map { |datum| User.new(datum) }
-    end
-    
-    def self.find_by_id(id)
-        user = QuestionsDatabase.instance.execute(<<-SQL, id)
-          SELECT
-             *
-          FROM
-             users
-          WHERE
-             id = ?
-        SQL
-        return nil unless user.length > 0
-    
-        User.new(user.first) # user is stored in an array!
-    end
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end
+  
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+    return nil unless user.length > 0
 
-    def initialize(options)
-        @id = options['id']
-        @fname = options['fname']
-        @lname = options['lname']
-    end
+    User.new(user.first) # user is stored in an array!
+  end
+
+  def self.find_by_name(fname, lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ? AND lname = ?
+    SQL
+    return nil unless user.length > 0
+
+    User.new(user.first) # user is stored in an array!
+  end
+
+  def initialize(options)
+      @id = options['id']
+      @fname = options['fname']
+      @lname = options['lname']
+  end
+end
+
+
+class Question
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+    return nil unless question.length > 0
+
+    Question.new(question.first)
+  end
+
+  def initialize(options)
+      @id = options['id']
+      @title = options['title']
+      @body = options['body']
+      @author_id = options['author_id']
+  end
+
+  def find_by_author_id(author_id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        author_id = ?
+    SQL
+    return nil unless questions.length > 0
+
+    questions.map { |question| Question.new(question) }
+  end
+
+  def find_by_title(title)
+    question = QuestionsDatabase.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        title = ?
+    SQL
+    return nil unless question.length > 0
+
+    Question.new(question.first)
+  end
 end
