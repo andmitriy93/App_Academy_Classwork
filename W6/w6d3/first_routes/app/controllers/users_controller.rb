54ca7af13f6977dc1c8f983ae312d4262a4c5063
Dmitrii An <an.dmitriy93@gmail.com>
2020-12-30 06:40:53 +0600

    render json params

--- W6/w6d3/first_routes/app/controllers/users_controller.rb
+++ W6/w6d3/first_routes/app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-  def index
-  end
-=======
     def index
         render plain: "I'm in the index action!"
     end
->>>>>>> 18d0fe24b7c37030d44773764ceb11cbdee5e640
+
+    def create
+      render json: params
+    end
 end
+
