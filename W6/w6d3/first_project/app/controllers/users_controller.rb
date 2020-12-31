e2b5e014bd6864acaac1eabea65bfd7f79cc4472
Justin Cheasty <cheasty@gmail.com>
2020-12-30 16:16:51 -0800

    Nearly completed

--- W6/w6d3/first_project/app/controllers/users_controller.rb
+++ W6/w6d3/first_project/app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        user = User.all
+        render json: user
     end
     
     def create
-        render json: params
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
-
+    
+    def show #find and show the user by their id
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
 end
+
