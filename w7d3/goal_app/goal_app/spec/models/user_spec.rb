4d1b1c0262354503f7fd607c00cd072285910ea2
annieyxu <annieyxu@berkeley.edu>
2021-01-06 14:42:39 -0800

    controller_spec opened

--- w7d3/goal_app/goal_app/spec/models/user_spec.rb
+++ w7d3/goal_app/goal_app/spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  let (:incomplete_user) { User.new(username: '', email: 'aa@aa.io', password: 'password') }
+	it 'validates the presence of username' do 
+		expect(incomplete_user).to_not be_valid
+  end #need to come back, wrong here
+
 end
