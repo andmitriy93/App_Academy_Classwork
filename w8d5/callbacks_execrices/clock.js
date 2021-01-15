16bfcb5260d148ed80fd6a1dab03fe9103620601
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-16 04:48:16 +0600

    working on _tick()

--- w8d5/callbacks_execrices/clock.js
+++ w8d5/callbacks_execrices/clock.js
@@ -1,12 +1,40 @@
 class Clock {
     constructor() {
-        const time = new Date();
+        this.time = new Date();
 
-        this.hours = time.getHours();
-        this.minutes = time.getMinutes();
-        this.seconds = time.getSeconds();
+        this.hours = this.time.getHours();
+        this.minutes = this.time.getMinutes();
+        this.seconds = this.time.getSeconds();
 
         this.printTime()
-        setInterval(this._tick(), 1000) // setInterval(fn, miliseconds)
+        setInterval(this._tick.bind(this), 1000) // setInterval(fn, miliseconds)
+        // setInterval(function() {
+        //     this.time.(this.seconds + 1);
+        //     this.printTime();
+        // }, 1000) // setInterval(fn, miliseconds)
     }
+
+    printTime() {
+        let timeFormat = [this.hours, this.minutes, this.seconds].join(":");
+        console.log(timeFormat);
+    }
+
+    _tick() {
+        // this.time.setSeconds(this.seconds + 1);
+        // while second < 60 => increment this.seconds
+        // while minutes <=60 => increment this.minutes
+        while (this.seconds < 59) {
+            this.seconds += 1;
+            
+            console.log(this.seconds);
+
+        }
+
+
+
+        this.printTime();
+    }
+
 }
+
+const clock = new Clock();
