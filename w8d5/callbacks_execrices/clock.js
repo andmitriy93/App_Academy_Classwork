eed200628e8e09d1b9b7670f993a5104e0a657e6
Itai Farhi <itaimf@gmail.com>
2021-01-15 16:23:14 -0800

    solve addNumbers

--- w8d5/callbacks_execrices/clock.js
+++ w8d5/callbacks_execrices/clock.js
@@ -1,60 +1,74 @@
-class Clock {
-    constructor() {
-        this.time = new Date();
+// class Clock {
+//     constructor() {
+//         this.time = new Date();
 
-        this.hours = this.time.getHours();
-        this.minutes = this.time.getMinutes();
-        this.seconds = this.time.getSeconds();
+//         this.hours = this.time.getHours();
+//         this.minutes = this.time.getMinutes();
+//         this.seconds = this.time.getSeconds();
 
-        this.printTime()
-        setInterval(this._tick.bind(this), 1000) // setInterval(fn, miliseconds)
-    }
+//         this.printTime()
+//         setInterval(this._tick.bind(this), 1000) // setInterval(fn, miliseconds)
+//     }
 
-    printTime() {
-        let timeFormat = [this.hours, this.minutes, this.seconds].join(":");
-        console.log(timeFormat);
-    }
+//     printTime() {
+//         let timeFormat = [this.hours, this.minutes, this.seconds].join(":");
+//         console.log(timeFormat);
+//     }
 
-    _tick() {
-        this._incrementSeconds();
+//     _tick() {
+//         this._incrementSeconds();
 
-        this.printTime();
+//         this.printTime();
+//     };
+
+//     _incrementSeconds() {
+//         this.seconds++;
+//         if (this.seconds === 60) {
+//             this.seconds = 0;
+//             this._incrementMinutes();
+//         }
+
+//     };
+
+//     _incrementMinutes() {
+//         this.minutes++;
+//         if (this.minutes === 60) {
+//             this.minutes = 0;
+//             this._incrementHours();
+//         }
+//     };
+
+//     _incrementHours() {
+//         this.hours++;
+//         if (this.hours === 24) {
+//             this.hours = 0;
+//         }  
+//     };
+
+// }
+
+// const clock = new Clock();
+
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let addNumbers = function(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question("Enter a number", function(num) {
+            const num1 = parseInt(num);
+            sum += num1;
+            console.log(sum);
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        });
+    } else if (numsLeft === 0) {
+        completionCallback(sum);
+        reader.close();
     };
+};
 
-    _incrementSeconds() {
-        this.seconds++;
-        if (this.seconds === 60) {
-            this.seconds = 0;
-            this._incrementMinutes();
-        }
-
-    };
-
-    _incrementMinutes() {
-        this.minutes++;
-        if (this.minutes === 60) {
-            this.minutes = 0;
-            this._incrementHours();
-        }
-    };
-
-    _incrementHours() {
-        this.hours++;
-        if (this.hours === 24) {
-            this.hours = 0;
-        }  
-    };
-
-}
-
-const clock = new Clock();
-
-
-let addNumbers = function() {
-    const readline = require('readline');
-
-    const reader = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-}
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
