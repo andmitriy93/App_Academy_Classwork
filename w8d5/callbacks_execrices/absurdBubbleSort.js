19113a9b33d37cbd58599d164911fce3ba4501a4
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-16 06:43:59 +0600

    Working on absurdBubbleSort

---
+++ w8d5/callbacks_execrices/absurdBubbleSort.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const absurbBubbleSort = function (arr, sortCompletionCallback) {
+    const askIfGreaterThan = function (el1, el2, callback) {
+        reader.question("Is element_1 > element_2", function (str) {
+            (str === 'yes') ? callback.call(true) : callback.call(false);
+        });
+        reader.close();
+        // let sorted = false;
+        // while (sorted === false) {
+        //     sorted = true;
+            // reader.question("Is element_1 > element_2", function (str) {
+            //     (str === 'yes') ? callback.call(true) : callback.call(false);
+            // });
+        // }
+    }
+}
+
+
+
+absurbBubbleSort([5,1,10,2], () => {
+    console.log(`${callback.call}`)
+})
