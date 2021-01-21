a0bbd3bbd21eb005e4c9dbce1b8ed64fe399d716
AlexeyS <xengawe@gmail.com>
2021-01-21 14:18:46 -0800

    work on twitter.js

--- w9d4/skeleton/frontend/follow_toggle.js
+++ w9d4/skeleton/frontend/follow_toggle.js
@@ -1,9 +1,14 @@
+const Twitter = require('./twitter.js')
+
 class FollowToggle {
     constructor($el) {
         this.userId = $el.data("user-id");
         this.followState = $el.data("initial-follow-state");
+
+        render();
     }
 
 }
+const user1 = new FollowToggle($el);
 
 module.exports = FollowToggle;
---
+++ w9d4/skeleton/frontend/twitter.js
@@ -0,0 +1,20 @@
+const FollowToggle = require('./follow_toggle.js')
+
+// const = Twitter {
+
+  $(document).ready(function() {
+    $('.follow-toggle').each(constructor(i))
+  
+    function render() {
+      if (this.followState === 'unfollowed') {
+        console.log("Follow!");
+      } else if (this.followState === 'followed') {
+        console.log("Unfollow!");
+      }
+    } 
+  })
+// }
+
+
+
+module.export = Twitter; 
