6d8b80b88b05ca2c68c16249c5153711eb4c7966
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-26 14:59:15 -0800

    Worked on getting our clock to render

--- w10d2/skeleton/frontend/clock.jsx
+++ w10d2/skeleton/frontend/clock.jsx
@@ -3,15 +3,30 @@ import React from 'react';
 
 class Clock extends React.Component {
 
+    constructor(props) {
+        super(props);
+        let date = Array.from(new Date()).join(" ");
+        this.state = { date };
+        // this.tick();
+        setInterval(this.tick(), 1000);
 
+
+        this.render = this.render.bind(this);
+    }
+    
+    tick() {
+        this.setState({date: Array.from(new Date()).join(" ") }, this.render);
+    }
 
     render(){
         return (
             <>
-                <h1>Printed Clock</h1>
+                <h1>{this.state.date}</h1>
             </>
         )
     }
+
+
 }
 
 export default Clock;
