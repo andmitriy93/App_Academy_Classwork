a18c8ad06d90adee5f5ef959ecf54a57b5d15c5d
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-27 04:29:02 +0600

    Finished Phase1 working on Phase2 Clock

---
+++ w10d2/skeleton/frontend/clock.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Clock extends React.Component {
+
+
+
+    render(){
+        return (
+            <>
+                <h1>Printed Clock</h1>
+            </>
+        )
+    }
+}
+
+export default Clock;
+// module.export = Clock;
