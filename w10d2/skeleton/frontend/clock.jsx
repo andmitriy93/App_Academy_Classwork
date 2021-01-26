feace00f530d7b7202e5cfb930e7470de73dd31e
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-27 05:40:04 +0600

    Working on styling

--- w10d2/skeleton/frontend/clock.jsx
+++ w10d2/skeleton/frontend/clock.jsx
@@ -5,23 +5,49 @@ class Clock extends React.Component {
 
     constructor(props) {
         super(props);
-        let date = Array.from(new Date()).join(" ");
+        let date = new Date();
         this.state = { date };
         // this.tick();
-        setInterval(this.tick(), 1000);
 
-
+        this.tick = this.tick.bind(this);
         this.render = this.render.bind(this);
     }
     
     tick() {
-        this.setState({date: Array.from(new Date()).join(" ") }, this.render);
+        this.setState({date: new Date() }, () => this.render());
+        this.id = this.state.date.getTime();
     }
 
+    componentDidMount() {
+        setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount() {
+        this.id = '';
+    }
+
+
+
     render(){
+        const month = [" January", " February", " March", " April", " May", " June", " July", " August", " September", " October", " November", " December"]
+        // debugger
         return (
             <>
-                <h1>{this.state.date}</h1>
+                <h1>Clock</h1>
+                <div className='clock'>
+                    <h2>Date</h2>
+                    <div>
+                        {this.state.date.getDate()} 
+                        {month[this.state.date.getMonth()]},  
+                        {this.state.date.getFullYear()}
+                    </div>
+                    <h2>Time</h2>
+                    <div>
+                        {this.state.date.getHours()}: 
+                        {this.state.date.getMinutes()}: 
+                        {this.state.date.getSeconds()}
+                    </div>
+                </div>
             </>
         )
     }
@@ -30,4 +56,3 @@ class Clock extends React.Component {
 }
 
 export default Clock;
-// module.export = Clock;
