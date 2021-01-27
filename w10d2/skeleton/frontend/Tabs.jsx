48fcb49ae7192abb4f8f4b9d63e270ad9fbac4da
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-27 07:21:02 +0600

    working on Phase 3 tabs widget

--- w10d2/skeleton/frontend/Tabs.jsx
+++ w10d2/skeleton/frontend/Tabs.jsx
@@ -1,5 +1,32 @@
-import react from 'react';
+import React from 'react';
 
-const Tabs = (props) => {
-    
+
+class Tabs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {id: 0};  
+    }
+
+    render() {
+        const titles = [];
+        const tabs = this.props.tabs
+        tabs.forEach(tab => {
+            // let title = createElement('li');
+            title.innerHTML = <h1>{tab.title}</h1>;
+            titles.push(title)
+        })
+        return (
+            <>
+                <ul>
+                    {titles}
+                </ul>
+                <article>
+                    {tabs[this.state.id].content}
+                </article>
+            </>
+        )
+    }
+
 };
+
+export default Tabs;
--- w10d2/skeleton/frontend/widgets.jsx
+++ w10d2/skeleton/frontend/widgets.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Congrats from '../congrats';
 import Clock from './clock';
+import Tabs from './tabs'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<Clock/>, root);
+  const tabs = [{title: 1, content: 'some content'}, {title: 2, content: 'some 2 content'}, {title: 3, content: 'some 3 content'}];
+  ReactDOM.render(<Tabs tabs={tabs}/>, root);
 });
