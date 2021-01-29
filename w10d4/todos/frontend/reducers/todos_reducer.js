8d6001a2a11d49376d66e22992a40c3187c0c365
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-29 07:59:06 +0600

    Working on Phase 2

--- w10d4/todos/frontend/reducers/todos_reducer.js
+++ w10d4/todos/frontend/reducers/todos_reducer.js
@@ -1,3 +1,6 @@
+import RECEIVE_TODOS from '../actions/todo_actions';
+import RECEIVE_TODO from '../actions/todo_actions';
+
 const initialState = {
     1: {
       id: 1,
@@ -12,12 +15,28 @@ const initialState = {
       done: true
     }
   };
+
+//   action.todo = { id: 3, title: "study", body: "js", done: false}
   
   const todosReducer = (state = initialState, action) => {
-    switch(action.type) {
-        default: 
-            return state;
-    }
+      Object.freeze(state);
+      const newState = {};
+
+        switch(action.type) {
+            case RECEIVE_TODOS:
+                //todos is an array
+                //itterate throught it and assign 
+                action.todos.forEach(todo => {
+                    newState[todo.id] = todo;
+                });
+                return newState;
+            case RECEIVE_TODO:
+
+                newState[action.todo.id] = action.todo;
+                return [...state, newState]
+            default: 
+                return state;
+            }
   };
 
 
