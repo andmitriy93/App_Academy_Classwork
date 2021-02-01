89c9ff1ff27659e9c253a83d8df99fd2e98eb1ff
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-02 04:36:16 +0600

    todo Part1 phase 2 complited

--- w10d4/todos/frontend/actions/todo_actions.js
+++ w10d4/todos/frontend/actions/todo_actions.js
@@ -1,17 +1,24 @@
-const RECEIVE_TODOS = 'RECEIVE_TODOS'
-const RECEIVE_TODO = 'RECEIVE_TODO'
-
+const RECEIVE_TODOS = "RECEIVE_TODOS";
+const RECEIVE_TODO = "RECEIVE_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
 
 export const receiveTodos = (todos) => {
-    return {
-        type: RECEIVE_TODOS,
-        todos,
-    };
-}
+  return {
+    type: RECEIVE_TODOS,
+    todos,
+  };
+};
 
 export const receiveTodo = (todo) => {
-    return {
-        type: RECEIVE_TODO,
-        todo,
-    };
-}
+  return {
+    type: RECEIVE_TODO,
+    todo,
+  };
+};
+
+export const removeTodo = (todo) => {
+  return {
+    type: REMOVE_TODO,
+    todo,
+  };
+};
--- w10d4/todos/frontend/reducers/todos_reducer.js
+++ w10d4/todos/frontend/reducers/todos_reducer.js
@@ -1,44 +1,43 @@
-import RECEIVE_TODOS from '../actions/todo_actions';
-import RECEIVE_TODO from '../actions/todo_actions';
+import RECEIVE_TODOS from "../actions/todo_actions";
+import RECEIVE_TODO from "../actions/todo_actions";
 
 const initialState = {
-    1: {
-      id: 1,
-      title: "wash car",
-      body: "with soap",
-      done: false
-    },
-    2: {
-      id: 2,
-      title: "wash dog",
-      body: "with shampoo",
-      done: true
-    }
-  };
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false,
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true,
+  },
+};
 
 //   action.todo = { id: 3, title: "study", body: "js", done: false}
-  
-  const todosReducer = (state = initialState, action) => {
-      Object.freeze(state);
-      const newState = {};
 
-        switch(action.type) {
-            case RECEIVE_TODOS:
-                //todos is an array
-                //itterate throught it and assign 
-                action.todos.forEach(todo => {
-                    newState[todo.id] = todo;
-                });
-                return newState;
-            case RECEIVE_TODO:
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  const newState = {};
 
-                newState[action.todo.id] = action.todo;
-                return [...state, newState]
-            default: 
-                return state;
-            }
-  };
-
-
+  switch (action.type) {
+    case RECEIVE_TODOS:
+      action.todos.forEach((todo) => {
+        newState[todo.id] = todo;
+      });
+      return newState;
+    case RECEIVE_TODO:
+      newState = Object.assign({}, state);
+      return (newState[action.todo.id] = action.todo);
+    case REMOVE_TODO:
+      newState = Object.assign({}, state);
+      delete newState[action.todo.id];
+      return newState;
+    default:
+      return state;
+  }
+};
 
 export default todosReducer;
