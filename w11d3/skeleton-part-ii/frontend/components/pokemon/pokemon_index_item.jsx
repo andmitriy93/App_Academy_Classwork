73f265a0fb378418a84400f7347034902eeb6c45
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-04 05:32:03 +0600

    working on Link tag phase 4

--- w11d3/skeleton-part-ii/frontend/components/pokemon/pokemon_index_item.jsx
+++ w11d3/skeleton-part-ii/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class PokemonIndexItem extends React.Component{
   constructor(props) {
@@ -8,9 +9,11 @@ class PokemonIndexItem extends React.Component{
   render() {
     return (
       <li>
+        <Link to="/pokemon/pokemonId">
         <span>{this.props.pokemon.id}</span>
         <img src={this.props.pokemon.imageUrl} />
         <span>{this.props.pokemon.name}</span>
+        </Link>
       </li>
     )
   }  
